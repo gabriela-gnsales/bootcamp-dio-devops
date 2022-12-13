c5039a54b0b1e43247ced4afecc03fd6a6b7ba47
gabriela-gnsales <gnsales.gabriela@gmail.com>
2022-12-13 18:17:17 -0300

    :sparkles: Adicionando desafio de projeto Linux - IaC

---
+++ linux/desafio-projeto-iac/iac.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Criando diretórios..."
+
+mkdir /publico /adm /ven /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123)
+
+# outro jeito de adicionar usuários aos grupos
+echo "Adicionando usuários aos grupos..."
+
+usermod GRP_SEC josefina
+usermod GRP_SEC amanda
+usermod GRP_SEC rogerio
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Fim..."
