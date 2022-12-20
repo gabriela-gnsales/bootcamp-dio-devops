8de783ef344b09c77b23f11c53262296760d4135
gabriela-gnsales <gnsales.gabriela@gmail.com>
2022-12-20 11:14:22 -0300

    :sparkles: Adicionando desafio de projeto 1 Kubernetes

---
+++ kubernetes/desafio-projeto-1/script.bat
@@ -0,0 +1,17 @@
+echo "Criando as imagens..."
+
+docker build -t gabrielagns/projeto-backend:1.0 backend/.
+docker build -t gabrielagns/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens..."
+
+docker push gabrielagns/projeto-backend:1.0
+docker push gabrielagns/projeto-database:1.0 
+
+echo "Criando serviços no cluster kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
---
+++ kubernetes/desafio-projeto-2/readme.md
@@ -0,0 +1,7 @@
+# Desafio de projeto
+
+## Criando um Pipeline de Deploy com GitLab e Kubernetes
+
+Neste projeto será criado um pipeline de deploy de uma aplicação com cenários de produção de imagens com Docker e criação dos deployments em um cluster kubernetes em nuvem utilizando o GCP.
+
+### Definições
